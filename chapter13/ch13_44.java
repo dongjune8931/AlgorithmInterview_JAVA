71a3e50264a4481a08613b94ffd4fea0769335cb
Dongjun <ldj990517@gmail.com>
2025-07-14 06:55:21 +0900

    ch13

--- chapter13/ch13_44.java
+++ chapter13/ch13_44.java
@@ -1,2 +1,68 @@
-package com.example.chapter13;public class ch13_44 {
+package com.example.chapter13;
+
+import java.util.AbstractMap;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Map.Entry : 하나의 key-value 쌍을 나타낼 때 사용
+ * 보통 Map을 반복문으로 돌릴 때 쓰임
+ * map.entrySet()
+ * entry.getKey()
+ * entry.getValue()
+ *
+ * [참고]
+ * 자바에서 Map.Entry 는 인터페이스라 직접 new로 만들수 없음.
+ * new AbstractMap.SimpleEntry<>(K, 0) 이런식으로 만들어야 한다.
+ */
+
+public class ch13_44 {
+
+	public int networkDelayTime(int[][] times, int n, int k){
+
+		Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+		for (int[] time : times) {
+			graph.putIfAbsent(time[0], new HashMap<>());
+			graph.get(time[0]).put(time[0], time[1]);
+		}
+		//<도착지, 소요시간>
+		Queue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(Map.Entry.comparingByValue());
+		pq.add(new AbstractMap.SimpleEntry<>(k, 0));
+
+		Map<Integer, Integer> dist = new HashMap<>();
+		while (!pq.isEmpty()) {
+			//소요 시간이 가장 빠른 값 추출
+			Map.Entry<Integer, Integer> cur = pq.poll();
+			//우선 순위 큐의 항목이 Map.Entry이므로 순회 과정없이
+			//간단히 getKey,Value로 조회 가능
+			int u = cur.getKey();
+			int dist_u = cur.getValue();
+
+			//u 지점까지의 소요 시간이 아직 계산되지 않았다면 처리하자
+			if(!dist.containsKey(u)){
+				dist.put(u, dist_u);
+
+				//u지점을 출발지로 한 경로 있으면 처리
+				if (graph.containsKey(u)) {
+					for (Map.Entry<Integer, Integer> v : graph.get(u).entrySet()) {
+						//u 지점까지 소요시간 + u 지점을 출발지로 한 도착지까지의 소요시간
+						int alt = dist_u + v.getValue();
+						//우선순위 큐에 (도착지, 소요 시간)을 삽입
+						pq.add(new AbstractMap.SimpleEntry<>(v.getKey(), alt));
+					}
+				}
+			}
+
+
+		}
+		if(dist.size()==n){
+			return Collections.max(dist.values());
+		}
+		return -1;
+
+	}
+
 }
--- chapter13/ch13_45.java
+++ chapter13/ch13_45.java
@@ -1,2 +1,54 @@
-package com.example.chapter13;public class ch13_45 {
+package com.example.chapter13;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class ch13_45 {
+
+	public int findCheapestPrice(int n, int[][] flights,int src, int dst, int k){
+		Map<Integer, Map<Integer, Integer>> graph = new HashMap<>();
+		for (int[] flight : flights) {
+			graph.putIfAbsent(flight[0], new HashMap<>());
+			graph.get(flight[0]).put(flight[0], flight[1]);
+		}
+		//(도착지, 비용, 진행 경로)
+		Queue<List<Integer>> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.get(1)));
+		pq.add(Arrays.asList(src, 0, 0));
+
+		//타임아웃을 방지하기 우히ㅏㄴ 한 번 방문한 경로는 저장해두는 맵
+		Map<Integer, Integer> visited = new HashMap<>();
+
+		while (!pq.isEmpty()) {
+			List<Integer> cur = pq.poll();
+			int u=cur.get(0);
+			int price_u = cur.get(1);
+			int k_visited=cur.get(2);
+
+			if (u == dst) {
+				return price_u;
+			}
+			visited.put(u, k_visited);
+
+			if (k_visited <= k) {
+				k_visited+=1;
+				//u 지점을 출발지로 한 경로가 있다면 시작
+				if (graph.containsKey(u)) {
+					for(Map.Entry<Integer,Integer> v:graph.get(u).entrySet()){
+						if (!visited.containsKey(v.getKey()) || k_visited < visited.get(v.getKey())) {
+							int alt= price_u+v.getValue();
+							pq.add(Arrays.asList(v.getKey(), alt, k_visited));
+						}
+
+					}
+				}
+			}
+		}
+		return -1;
+	}
 }
