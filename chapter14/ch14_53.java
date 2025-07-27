fb27bd6affba5f708f7e9f8ef0260c80a7139087
Dongjun <ldj990517@gmail.com>
2025-07-27 23:33:16 +0900

    ch14 53

--- chapter14/ch14_53.java
+++ chapter14/ch14_53.java
@@ -14,6 +14,23 @@ public class ch14_53 {
 
 	}
 
+	public int dfs(TreeNode node){
+		if (node == null) {
+			return 0;
+		}
+
+		int left = dfs(node.left);
+		int right = dfs(node.right);
+
+		if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+			return -1;
+		}
+		return Math.max(left,right)+1;
+	}
+	public boolean isBalanced(TreeNode root){
+		return dfs(root)!=-1;
+	}
+
 
 
 
